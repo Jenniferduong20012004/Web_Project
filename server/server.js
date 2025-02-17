50062d809b9da9d5ea7281aad222892eec5c664e
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2025-02-17 22:36:03 +0700

    backend

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require ('express')
+const app = express()
+app.get ("/api", (req, res) =>{
+    res.json ({"users": ["userOne", "userTwo", "userThree"]})
+})
+app.listen (5000, () => {console.log ("5000")})
