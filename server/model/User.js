d8bd114111257644d9445b00d4606654c45471d6
thaortrinh <nghthtr5536@gmail.com>
2025-03-01 23:24:19 +0700

    Chin | separate signup route

--- server/model/User.js
+++ server/model/User.js
@@ -1,15 +1,59 @@
+// const pool = require("../db/connect");
+
+// const createUser = (userName, email, passWord, callback) => {
+//   const query = "INSERT INTO User (name, email, password) VALUES (?, ?, ?)";
+
+//   pool.query(query, [userName, email, passWord], (err, results) => {
+//     if (err) {
+//       console.error("Error when register/sign-up:", err);
+//       return callback(err, null);
+//     }
+//     return callback(null, results);
+//   });
+// };
+
+// module.exports = { createUser };
+
 const pool = require("../db/connect");
 
-const createUser = (userName, email, passWord, callback) => {
-  const query = "INSERT INTO User (name, email, password) VALUES (?, ?, ?)";
+class User {
 
-  pool.query(query, [userName, email, passWord], (err, results) => {
-    if (err) {
-      console.error("Error when register/sign-up:", err);
-      return callback(err, null);
-    }
-    return callback(null, results);
-  });
-};
+  static create(userData, callback) {
+    const { name, email, password } = userData;
+    const query = "INSERT INTO User (name, email, password) VALUES (?, ?, ?)";
 
-module.exports = { createUser };
+    pool.query(query, [name, email, password], (err, results) => {
+      if (err) {
+        console.error("Error creating user:", err);
+        return callback(err, null);
+      }
+      return callback(null, results);
+    });
+  }
+
+  static findByEmail(email, callback) {
+    const query = "SELECT * FROM User WHERE email = ?";
+    
+    pool.query(query, [email], (err, results) => {
+      if (err) {
+        console.error("Error finding user by email:", err);
+        return callback(err, null);
+      }
+      return callback(null, results.length > 0 ? results[0] : null);
+    });
+  }
+
+  static findById(userId, callback) {
+    const query = "SELECT * FROM User WHERE userId = ?";
+    
+    pool.query(query, [userId], (err, results) => {
+      if (err) {
+        console.error("Error finding user by ID:", err);
+        return callback(err, null);
+      }
+      return callback(null, results.length > 0 ? results[0] : null);
+    });
+  }
+}
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,47 +1,14 @@
 const express = require("express");
-const pool = require("./db/connect");
 const cors = require("cors");
-const { createUser } = require("./model/User");
+const routes = require("./routes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API Signup
-app.post("/signup", (req, res) => {
-  const { userName, email, passWord, confirmPassword } = req.body;
+app.use("/", routes);
 
-  if (!userName || !email || !passWord || !confirmPassword) {
-      return res.status(400).json({ error: true, message: "Please fill in all fields!" });
-  }
-
-  if (passWord !== confirmPassword) {
-      return res.status(400).json({ error: true, message: "Passwords do not match!" });
-  }
-
-  pool.query("SELECT * FROM User WHERE email = ?", [email], (err, results) => {
-      if (err) {
-          return res.status(500).json({ error: true, message: "System error!" });
-      }
-
-      if (results.length > 0) {
-          return res.status(400).json({ error: true, message: "Email already exists!" });
-      }
-
-      createUser(userName, email, passWord, (err, data) => {
-          if (err) {
-              return res.status(500).json({ error: true, message: "Error creating account!" });
-          }
-          console.log("Signup successful!");
-          res.status(201).json({ success: true, message: "Signup successful!" });
-      });
-  });
-});
-
-app.post("/login", (req, res) => {
-  res.status(501).json({ message: "Not implemented yet." });
-});
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
