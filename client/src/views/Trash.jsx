cfce6a514a32056a85e9ab3a371ad22f2202ee3b
Bngoc <luuboingoc1235@gmail.com>
2025-04-28 15:13:03 +0700

    feat: add TrashBin component and update Trash view
    - Added a new TrashBin.jsx component to manage trash functionality.
    - Updated Trash.jsx to integrate the new TrashBin component.

--- client/src/views/Trash.jsx
+++ client/src/views/Trash.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Sidebar from "../component/Sidebar";
 import Navbar from "../component/Navbar";
+import TrashBin from "../component/trash/TrashBin";
 
 function Trash() {
-  return (
-    <div className="w-full min-h-screen flex flex-col">
-      <div className="fixed top-0 right-0 left-0 z-20">
-        <Navbar />
-      </div>
+    return (
+        <div className="w-full min-h-screen flex flex-col">
+            <div className="fixed top-0 right-0 left-0 z-20">
+                <Navbar />
+            </div>
 
-      <div className="fixed left-0 top-16 h-screen z-10">
-        <Sidebar />
-      </div>
+            <div className="fixed left-0 top-16 h-screen z-10">
+                <Sidebar />
+            </div>
 
-      <div className="flex-1 flex flex-col !mt-16 bg-gray-50">
-        <div className="flex-1 !p-8 md:p-6 overflow-auto !ml-50">
-          {/* TRASH CONTENT */}
-          THIS IS TRASH BIN!
+            <div className="flex-1 flex flex-col !mt-16 bg-gray-50">
+                <div className="flex-1 !p-8 md:p-6 overflow-auto !ml-50">
+                    {/* TRASH BIN */}
+                    <TrashBin />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Trash;
