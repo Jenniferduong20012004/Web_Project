902bb777bbf1372a69fc3dac0c76316f71dd609d
thaortrinh <nghthtr5536@gmail.com>
2025-05-08 02:37:07 +0700

    chin2 | add Edit & Delete WS api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const authRoutes = require("./authRoutes");
-const homePageRoute = require ("./homePageRoute")
-const dashBoardRoute = require ("./dashBoardRoute")
-const addWorkSpaceRoute = require ("./addWorkspaceRoute")
-
+const homePageRoute = require("./homePageRoute");
+const dashBoardRoute = require("./dashBoardRoute");
+const addWorkSpaceRoute = require("./addWorkspaceRoute");
+const updateWorkSpaceRoute = require("./updateWorkSpaceRoute");
+const deleteWorkSpaceRoute = require("./deleteWorkSpaceRoute");
 
 router.use("/", authRoutes);
-router.use ("/", homePageRoute);
-router.use ("/getDashBoard", dashBoardRoute);
-router.use ("/addWorkSpace", addWorkSpaceRoute);
+router.use("/", homePageRoute);
+router.use("/getDashBoard", dashBoardRoute);
+router.use("/addWorkSpace", addWorkSpaceRoute);
+router.use("/updateWorkSpace", updateWorkSpaceRoute);
+router.use("/deleteWorkSpace", deleteWorkSpaceRoute);
 module.exports = router;
