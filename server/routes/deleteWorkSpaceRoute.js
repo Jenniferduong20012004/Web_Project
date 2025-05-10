9a000ce6a12fe105c4d6d419c243e158812bd4ba
thaortrinh <nghthtr5536@gmail.com>
2025-05-11 01:34:53 +0700

    chin2 | format routes

--- server/routes/deleteWorkSpaceRoute.js
+++ server/routes/deleteWorkSpaceRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const deleteWorkSpaceController = require("../controllers/deleteWorkSpaceController");
 
-router.post("/", deleteWorkSpaceController.deleteWorkSpace);
+router.post("/deleteWorkSpace", deleteWorkSpaceController.deleteWorkSpace);
 
 module.exports = router;
