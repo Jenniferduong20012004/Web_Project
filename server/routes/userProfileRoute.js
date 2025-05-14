a24b512f260f362db2aa45222e8f26f78d5b2f1e
Itsjennyyahh <56812916+Jenniferduong20012004@users.noreply.github.com>
2025-05-15 00:06:32 +0700

    ava

--- server/routes/userProfileRoute.js
+++ server/routes/userProfileRoute.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const multer  = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage })
 const userProfileController = require("../controllers/userProfileController");
 router.post("/getProfile", userProfileController.userProfileControl);
 router.post ("/updateProfile", userProfileController.updateUsername)
+router.post ("/addProfilePicture", upload.single("uploaded_file") ,userProfileController.updatePicture);
 module.exports = router;
